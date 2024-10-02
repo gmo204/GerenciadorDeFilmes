ce0d5593d1d6a7733dc5f8dfbd2d2231ea3db38a
Gilberto Mota <gilbertojr204@gmail.com>
2024-10-02 14:43:28 -0300

    configuracao &:hover do card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { ListagemFilmesComponent } from "./components/listagem-filmes/listagem-f
 import { LOCALE_ID } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import localePtBr from '@angular/common/locales/pt';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ListagemFilmesComponent],
+  imports: [RouterOutlet, NavbarComponent],
   providers: [ {provide: LOCALE_ID, useValue: 'pt-BR'} ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ListagemFilmesComponent } from './components/listagem-filmes/listagem-filmes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'filmes', pathMatch: 'full'},
+  {path: 'filmes', component: ListagemFilmesComponent},
+];
